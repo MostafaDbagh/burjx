70dd4d7169f8457539436f7b451915c8bc3eb053
Mostafa-dbagh <mostafa@burjx.com>
2025-05-12 09:51:27 +0400

    change backgrund

--- src/components/ui/background/index.jsx
+++ src/components/ui/background/index.jsx
@@ -4,13 +4,13 @@ export const HeaderBackground  = ({t}) =>{
     return (
         <div className="background">
         <div className=" d-flex flex-column align-items-cetner justify-content-end padding-top-down pb-5">
-          <h3 className="px-5 flexFontHeader text-white">
+          <h3 className="px-5 flexFontHeader">
             {t("home.lets")} <br /> {t("home.explore")} <br />
             <span className=" text-bold-pink"> {t("home.toma")}</span> <br />
             {t("home.boutique")}
           </h3>
           <div className="flexFontDesc">
-            <p className=" py-2 text-white px-3 ">{t("home.title")}</p>
+            <p className=" py-2 text-black px-3 ">{t("home.title")}</p>
             <button className="primary-cta">{t("home.cta")}</button>
           </div>
         </div>
