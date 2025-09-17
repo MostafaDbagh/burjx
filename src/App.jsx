131a8c55d1d717460e8df646222b46b69c16820e
aboodTurkman <abood2002turkman@gmail.com>
2025-09-17 15:32:27 +0300

    create navbar and pages structure

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <>
-    <Nav+bar/>
+    <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/categories" element={<Categories />} />
